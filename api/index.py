f7fd2709d9e9e12ebaca2d0c3d706fd774057a67
Ju / smwhr <ju@smwhr.net>
2023-01-11 13:46:06 +0100

    migrate on load

--- api/index.py
+++ api/index.py
@@ -1,5 +1,3 @@
 from themis import create_app
-from flask_migrate import upgrade
 
 app = create_app()
-upgrade()
--- themis/extensions.py
+++ themis/extensions.py
@@ -1,8 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-from flask_migrate import Migrate
-migrate = lambda app: Migrate(app, db)
+from flask_migrate import Migrate, upgrade
+def migrate(app):
+    m = Migrate(app, db)
+    with app.app_context():
+        upgrade()
+    return m
 
 from flask_login import LoginManager
 login_manager = LoginManager()
